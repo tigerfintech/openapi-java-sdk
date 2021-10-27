58f943a5e94e6b943d07ce6943fd8b5b789ab014
luojiaqi <luojiaqi@tigerbrokers.com>
2021-10-27 14:51:45 +0800

    feat: option chain query v2

---
+++ src/main/java/com/tigerbrokers/stock/openapi/client/https/domain/option/model/OptionChainV2Model.java
@@ -0,0 +1,41 @@
+/*
+ * TigerBrokers
+ * Copyright (C) 2014-2021 All Rights Reserved.
+ */
+package com.tigerbrokers.stock.openapi.client.https.domain.option.model;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.tigerbrokers.stock.openapi.client.https.domain.ApiModel;
+import java.util.List;
+
+public class OptionChainV2Model extends ApiModel {
+
+  @JSONField(name = "option_basic")
+  private List<OptionChainModel> optionBasic;
+  @JSONField(name = "option_filter")
+  private OptionChainFilterModel optionFilter;
+
+  public List<OptionChainModel> getOptionBasic() {
+    return optionBasic;
+  }
+
+  public void setOptionBasic(List<OptionChainModel> optionBasic) {
+    this.optionBasic = optionBasic;
+  }
+
+  public OptionChainFilterModel getOptionFilter() {
+    return optionFilter;
+  }
+
+  public void setOptionFilter(OptionChainFilterModel optionFilter) {
+    this.optionFilter = optionFilter;
+  }
+
+  @Override
+  public String toString() {
+    return "OptionChainV2Model{" +
+        "optionBasic=" + optionBasic +
+        ", optionFilter=" + optionFilter +
+        '}';
+  }
+}
