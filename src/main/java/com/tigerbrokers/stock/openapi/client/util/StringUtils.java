39b6c9a068d7baefcfa5c3d7e0f4229c3297b22d
liutongping <liutongping@itiger.com>
2021-11-09 17:29:23 +0800

    修复java-sdk对期货下单需要区分环球/综合账号问题

--- src/main/java/com/tigerbrokers/stock/openapi/client/util/StringUtils.java
+++ src/main/java/com/tigerbrokers/stock/openapi/client/util/StringUtils.java
@@ -32,4 +32,20 @@ public abstract class StringUtils {
     }
     return result;
   }
+
+  public static boolean isNumeric(CharSequence cs) {
+    if (cs == null || cs.length() == 0) {
+      return false;
+    } else {
+      int sz = cs.length();
+
+      for(int i = 0; i < sz; ++i) {
+        if (!Character.isDigit(cs.charAt(i))) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+  }
 }
