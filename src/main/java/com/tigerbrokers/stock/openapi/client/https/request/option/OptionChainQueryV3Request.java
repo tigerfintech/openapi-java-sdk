34eb2f3d1aa3523b7995e1f77d18233d7f1f0b0d
luojiaqi <luojiaqi@tigerbrokers.com>
2021-11-12 16:30:56 +0800

    feat: option chain filter

--- src/main/java/com/tigerbrokers/stock/openapi/client/https/request/option/OptionChainQueryV3Request.java
+++ src/main/java/com/tigerbrokers/stock/openapi/client/https/request/option/OptionChainQueryV3Request.java
@@ -7,32 +7,32 @@ package com.tigerbrokers.stock.openapi.client.https.request.option;
 import com.tigerbrokers.stock.openapi.client.constant.ApiServiceType;
 import com.tigerbrokers.stock.openapi.client.https.domain.option.model.OptionChainFilterModel;
 import com.tigerbrokers.stock.openapi.client.https.domain.option.model.OptionChainModel;
-import com.tigerbrokers.stock.openapi.client.https.domain.option.model.OptionChainV2Model;
+import com.tigerbrokers.stock.openapi.client.https.domain.option.model.OptionChainV3Model;
 import com.tigerbrokers.stock.openapi.client.https.request.TigerCommonRequest;
 import com.tigerbrokers.stock.openapi.client.https.request.TigerRequest;
 import com.tigerbrokers.stock.openapi.client.https.response.option.OptionChainResponse;
 import java.util.ArrayList;
 import java.util.List;
 
-public class OptionChainQueryV2Request extends TigerCommonRequest implements TigerRequest<OptionChainResponse> {
+public class OptionChainQueryV3Request extends TigerCommonRequest implements TigerRequest<OptionChainResponse> {
 
-  public OptionChainQueryV2Request() {
-    setApiVersion(V2_0);
+  public OptionChainQueryV3Request() {
+    setApiVersion(V3_0);
     setApiMethodName(ApiServiceType.OPTION_CHAIN);
   }
 
-  public static OptionChainQueryV2Request of(OptionChainModel item, OptionChainFilterModel filter) {
+  public static OptionChainQueryV3Request of(OptionChainModel item, OptionChainFilterModel filter) {
     List<OptionChainModel> models = new ArrayList<>();
     models.add(item);
     return of(models, filter);
   }
 
-  public static OptionChainQueryV2Request of(List<OptionChainModel> items, OptionChainFilterModel filter) {
-    OptionChainV2Model v2Model = new OptionChainV2Model();
-    v2Model.setOptionFilter(filter);
-    v2Model.setOptionBasic(items);
-    OptionChainQueryV2Request request = new OptionChainQueryV2Request();
-    request.setApiModel(v2Model);
+  public static OptionChainQueryV3Request of(List<OptionChainModel> items, OptionChainFilterModel filter) {
+    OptionChainV3Model v3Model = new OptionChainV3Model();
+    v3Model.setOptionFilter(filter);
+    v3Model.setOptionBasic(items);
+    OptionChainQueryV3Request request = new OptionChainQueryV3Request();
+    request.setApiModel(v3Model);
     return request;
   }
 
