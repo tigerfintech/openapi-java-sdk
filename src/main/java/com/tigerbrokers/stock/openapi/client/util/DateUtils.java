229993c64bd160c3ef2787257d216a75d7a4411e
liutongping <liutongping@itiger.com>
2021-11-29 12:55:55 +0800

    去掉时区，设置毫秒时间

--- src/main/java/com/tigerbrokers/stock/openapi/client/util/DateUtils.java
+++ src/main/java/com/tigerbrokers/stock/openapi/client/util/DateUtils.java
@@ -16,7 +16,7 @@ import java.util.Date;
 public class DateUtils {
 
   private static final String FORMAT_FULL = "yyyy-MM-dd HH:mm:ss";
-  private static final String FORMAT_FULL_WITH_ZONE = "yyyy-MM-dd HH:mm:ss z";
+  private static final String FORMAT_FULL_WITH_ZONE = "yyyy-MM-dd HH:mm:ss.SSS";
   private static final String SUFFIX = " 00:00:00";
   private static final String FORMAT_DATE = "yyyy-MM-dd";
   public static final DateTimeFormatter DATE_FORMAT =
