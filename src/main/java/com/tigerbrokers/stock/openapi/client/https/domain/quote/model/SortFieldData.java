45bc3272b6b48efc78421cafd7e977913480d37c
JiaWen Li <lijiawen@itiger.com>
2022-11-08 14:24:13 +0800

    optz

--- src/main/java/com/tigerbrokers/stock/openapi/client/https/domain/quote/model/SortFieldData.java
+++ src/main/java/com/tigerbrokers/stock/openapi/client/https/domain/quote/model/SortFieldData.java
@@ -1,5 +1,8 @@
 package com.tigerbrokers.stock.openapi.client.https.domain.quote.model;
 
+import com.tigerbrokers.stock.openapi.client.struct.enums.FieldBelongType;
+import com.tigerbrokers.stock.openapi.client.struct.enums.SortDir;
+
 import java.io.Serializable;
 
 /**
@@ -13,7 +16,7 @@ public class SortFieldData implements Serializable {
   /** 排序属性 */
   private Integer fieldName;
   /** 排序属性所属类别 参考 FieldBelongType */
-  private Integer fieldType;
+  private FieldBelongType fieldType;
   /** SortDir 排序方向，默认不排序。 */
-  private int sortDir;
+  private SortDir sortDir;
 }
