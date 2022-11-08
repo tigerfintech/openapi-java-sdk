edb6ed9742501e878bba184b1864cabf95a00c32
JiaWen Li <lijiawen@itiger.com>
2022-11-08 17:21:43 +0800

    market scanner optz

--- src/main/java/com/tigerbrokers/stock/openapi/client/https/domain/quote/model/SortFieldData.java
+++ src/main/java/com/tigerbrokers/stock/openapi/client/https/domain/quote/model/SortFieldData.java
@@ -19,4 +19,74 @@ public class SortFieldData implements Serializable {
   private FieldBelongType fieldType;
   /** SortDir 排序方向，默认不排序。 */
   private SortDir sortDir;
+
+  public static SortFieldData.SortFieldDataBuilder builder() {
+    return new SortFieldData.SortFieldDataBuilder();
+  }
+
+  public Integer getFieldName() {
+    return this.fieldName;
+  }
+
+  public FieldBelongType getFieldType() {
+    return this.fieldType;
+  }
+
+  public SortDir getSortDir() {
+    return this.sortDir;
+  }
+
+  public void setFieldName(Integer fieldName) {
+    this.fieldName = fieldName;
+  }
+
+  public void setFieldType(FieldBelongType fieldType) {
+    this.fieldType = fieldType;
+  }
+
+  public void setSortDir(SortDir sortDir) {
+    this.sortDir = sortDir;
+  }
+
+  public SortFieldData(Integer fieldName, FieldBelongType fieldType, SortDir sortDir) {
+    this.fieldName = fieldName;
+    this.fieldType = fieldType;
+    this.sortDir = sortDir;
+  }
+
+  public SortFieldData() {
+  }
+
+  public static class SortFieldDataBuilder {
+    private Integer fieldName;
+    private FieldBelongType fieldType;
+    private SortDir sortDir;
+
+    SortFieldDataBuilder() {
+    }
+
+    public SortFieldData.SortFieldDataBuilder fieldName(Integer fieldName) {
+      this.fieldName = fieldName;
+      return this;
+    }
+
+    public SortFieldData.SortFieldDataBuilder fieldType(FieldBelongType fieldType) {
+      this.fieldType = fieldType;
+      return this;
+    }
+
+    public SortFieldData.SortFieldDataBuilder sortDir(SortDir sortDir) {
+      this.sortDir = sortDir;
+      return this;
+    }
+
+    public SortFieldData build() {
+      return new SortFieldData(this.fieldName, this.fieldType, this.sortDir);
+    }
+
+    @Override
+    public String toString() {
+      return "SortFieldData.SortFieldDataBuilder(fieldName=" + this.fieldName + ", fieldType=" + this.fieldType + ", sortDir=" + this.sortDir + ")";
+    }
+  }
 }
