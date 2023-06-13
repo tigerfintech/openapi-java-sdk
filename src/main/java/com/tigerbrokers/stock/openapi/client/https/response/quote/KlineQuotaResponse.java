0155b35826f56c576aa5a91482597b22ba0da883
liutongping <liutongping@itiger.com>
2023-06-13 20:32:10 +0800

    use QuotaItem

--- src/main/java/com/tigerbrokers/stock/openapi/client/https/response/quote/KlineQuotaResponse.java
+++ src/main/java/com/tigerbrokers/stock/openapi/client/https/response/quote/KlineQuotaResponse.java
@@ -1,7 +1,7 @@
 package com.tigerbrokers.stock.openapi.client.https.response.quote;
 
 import com.alibaba.fastjson.annotation.JSONField;
-import com.tigerbrokers.stock.openapi.client.https.domain.quote.item.MarketItem;
+import com.tigerbrokers.stock.openapi.client.https.domain.quote.item.QuotaItem;
 import com.tigerbrokers.stock.openapi.client.https.response.TigerResponse;
 import java.util.List;
 
@@ -10,15 +10,14 @@ import java.util.List;
  * Created by bean on 2023/06/13.
  */
 public class KlineQuotaResponse extends TigerResponse {
-
   @JSONField(name = "data")
-  private List<MarketItem> marketItems;
+  private List<QuotaItem> quotaItems;
 
-  public List<MarketItem> getMarketItems() {
-    return marketItems;
+  public List<QuotaItem> getQuotaItems() {
+    return quotaItems;
   }
 
-  public void setMarketItems(List<MarketItem> marketItems) {
-    this.marketItems = marketItems;
+  public void setQuotaItems(List<QuotaItem> quotaItems) {
+    this.quotaItems = quotaItems;
   }
 }
