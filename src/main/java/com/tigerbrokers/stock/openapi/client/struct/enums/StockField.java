3a93653597bd84c811d03484ed74f59762ae55b0
liutongping <liutongping@itiger.com>
2023-06-28 11:38:07 +0800

    更新注释

--- src/main/java/com/tigerbrokers/stock/openapi/client/struct/enums/StockField.java
+++ src/main/java/com/tigerbrokers/stock/openapi/client/struct/enums/StockField.java
@@ -6,126 +6,125 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Description: 简单属性
+ * Description: scanner base stock field
  *
  * @author kevin
  * @date 2022/10/27
  */
 public enum StockField {
-    /** 股票代码*，不能填区间上下限值。 */
-    /** 最新价*（精确到小数点后 3 位，超出部分会被舍弃）例如填写[10,20]值区间 */
+    /** Latest price* (accurate to 3 decimal places, the excess will be discarded) For example, fill in the value range of [10,20] */
     StockField_CurPrice(2, "latestPrice"),
-    /** 买入价（精确到小数点后 3 位，超出部分会被舍弃）例如填写[10,20]值区间 */
+    /** Purchase price (accurate to 3 decimal places, the excess will be discarded) For example, fill in the value range [10,20] */
     StockField_BidPrice(3, "bidPrice"),
-    /** 卖出价（精确到小数点后 3 位，超出部分会被舍弃）例如填写[10,20]值区间 */
+    /** Selling price (accurate to 3 decimal places, the excess will be discarded) For example, fill in the value range [10,20] */
     StockField_AskPrice(4, "askPrice"),
-    /** 今开价（精确到小数点后 3 位，超出部分会被舍弃）例如填写[10,20]值区间 */
+    /** Today's offer price (accurate to 3 decimal places, the excess will be discarded) For example, fill in the value range [10,20] */
     StockField_OpenPrice(5, "open"),
-    /** 昨收价（精确到小数点后 3 位，超出部分会被舍弃）例如填写[10,20]值区间 */
+    /** Yesterday's closing price (accurate to 3 decimal places, the excess will be discarded) For example, fill in the value range [10,20] */
     StockField_PreClosePrice(6, "preClose"),
-    /** 最高价 */
+    /** Highest price */
     StockField_HighPrice(7, "high"),
-    /** 最低价 */
+    /** Lowest price */
     StockField_LowPrice(8, "low"),
-    /** 盘前价*（精确到小数点后 3 位，超出部分会被舍弃）例如填写[10,20]值区间 */
+    /** Pre-market price* (accurate to 3 decimal places, the excess will be discarded) For example, fill in the [10,20] value range */
     StockField_HourTradingPrePrice(9, "hourTradingPrePrice"),
-    /** 盘后价*（精确到小数点后 3 位，超出部分会被舍弃）例如填写[10,20]值区间 */
+    /** After-hours price* (accurate to 3 decimal places, the excess will be discarded) For example, fill in the [10,20] value range */
     StockField_HourTradingAfterPrice(10, "hourTradingAfterPrice"),
-    /** 成交量* */
+    /** Volume* */
     StockField_Volume(11, "volume"),
-    /** 成交额* */
+    /** trading amount* */
     StockField_Amount(12, "amount"),
-    /** 流通股本* */
+    /** Circulating share capital* */
     StockField_FloatShare(13, "floatShares"),
-    /** 52周最高价格* */
+    /** 52 week maximum price* */
     StockField_Week52High(14, "week52High"),
-    /** 52周最低价格* */
+    /** Lowest price for 52 weeks* */
     StockField_Week52Low(15, "week52Low"),
-    /** 通市值* FloatMarketVal  自己计算 FloatShare* 当前价格 */
+    /** Market value* FloatMarketVal calculates FloatShare* current price */
     StockField_FloatMarketVal(16, "floatMarketCap"),
-    /** 总市值*  MarketVal  shares * 当前价格 */
+    /** total market value * MarketVal shares * current price */
     StockField_MarketValue(17, "marketValue"),
-    /** 盘前涨跌幅   (curPrice-盘前左收）自己计算 最新价-close / close */
+    /** Pre-market price rise and fall (curPrice-pre-market left close) self-calculated latest price-close / close */
     StockField_preHourTradingChangeRate(18, "preHourTradingChangeRate"),
-    /** 盘后涨跌幅 自己计算 */
+    /** Calculate the after-hours rise and fall by yourself */
     StockField_postHourTradingChangeRate(19, "postHourTradingChangeRate"),
-    /** 每股收益 滚动市盈率 TTM=过去12个月  Last Twelve Month  通过hermes获取 eps */
+    /** Earnings per share rolling price-earnings ratio TTM=last 12 months Last Twelve Month Get eps through hermes */
     StockField_ttm_Eps(20, "ttmEps"),
-    /** 量比*（精确到小数点后 3 位，超出部分会被舍弃）例如填写 [0.005,0.01] 值区间 */
+    /** Quantity ratio* (accurate to 3 digits after the decimal point, the excess will be discarded) For example, fill in the value range [0.005,0.01] */
     StockField_VolumeRatio(21, "volumeRatio"),
-    /** 委比*（精确到小数点后 3 位，超出部分会被舍弃）例如填写 [0.005,0.01] 值区间 */
+    /** Commission ratio* (accurate to 3 digits after the decimal point, the excess will be discarded) For example, fill in the value range [0.005,0.01] */
     StockField_BidAskRatio(22, "committee"),
-    /** 下次财报日期 * */
+    /** Next financial report date * */
     StockField_EarningDate(23, "earningDate"),
-    /** 市盈率* TTM（精确到小数点后 3 位，超出部分会被舍弃）例如填写 [0.005,0.01] 值区间 */
+    /** Price-earnings ratio* TTM (accurate to 3 decimal places, the excess will be discarded) For example, fill in the value range [0.005,0.01] */
     StockField_PeTTM(24, "peRate"),
-    /** 股息   hermes $ */
+    /** Dividend hermes $ */
     StockField_DividePrice(26, "dividePriceVal"),
-    /** 股息收益率 选股服务自身计算 */
+    /** Dividend yield is calculated by the stock selection service itself */
     StockField_DivideRate(27, "divideRateVal"),
-    /** 股票交易市场 */
+    /** stock market */
     StockField_Exchange(29, "exchange"),
-    /** 换手率*（精确到小数点后 3 位，超出部分会被舍弃）例如填写 [0.005,0.01] 值区间 */
+    /** Turnover rate* (accurate to 3 decimal places, the excess will be discarded) For example, fill in the value range [0.005,0.01] */
     StockField_TurnoverRate(30, "turnoverRate"),
-    /** 上市时间 */
+    /** Time to market */
     StockField_ListingDate(31, "listingDate"),
-    /** 总股本* */
+    /** Total share capital* */
     StockField_Share(33, "shares"),
-    /** 上市价格* */
+    /** listing price* */
     StockField_ListingPrice(34, "listingPrice"),
-    /** 最新价-发行价* */
+    /** Latest price - issue price* */
     StockField_DiffBetweenLastPriceAndListPrice(36, "DiffBetweenLastPriceAndListPrice"),
-    /** 每股收益 lyr=Last Year Ratio 静态市盈率 */
+    /** Earnings per share lyr=Last Year Ratio static price-earnings ratio */
     StockField_lyr_Eps(37, "lyrEps"),
-    /** 未平仓做空量 */
+    /** Open short position */
     StockField_Open_Short_Interest(38, "OpenShortInterestVal"),
-    /** 未平仓做空比例 = 未平仓做空量/总股本 */
+    /** Open short position ratio = open short position volume / total equity */
     StockField_Open_Short_Interest_Ratio(39, "OpenShortInterestRatio"),
-    /** 产权比率 = Liability/Equity 总负债/股东 */
+    /** Equity Ratio = Liability/Equity Total Liabilities/Shareholders */
     StockField_Equity_Ratio(40, "totalDebtToEquity"),
-    /** 权益乘数 = Asset/Equity */
+    /** Equity multiplier = Asset/Equity */
     StockField_Equity_Multiplier(41, "totalLiabilitiesToTotalAssets"),
-    /** 最新股东数 */
+    /** The latest number of shareholders */
     StockField_Holder_Nums(42, "holderNums"),
-    /** 最新股东户数增长率 */
+    /** The latest growth rate of the number of shareholders */
     StockField_Holder_Nums_Ratio(43, "holderRatio"),
-    /** 户均持股数量 */
+    /** The average number of shares held by each account */
     StockField_Per_Hold_Nums(44, "perHolderNums"),
-    /** 户均持股金额 */
+    /** The average amount of shares held by each account */
     StockField_Per_Hold_Money(45, "perHolderMoney"),
-    /** 户均持股数半年增长率 */
+    /** Semi-annual growth rate of the average number of shares held by each account */
     StockField_HalfYear_Holder_Nums_Ratio(46, "HalfYearholderRatio"),
-    /** 发行时间 - ETF */
+    /** Issue time - ETF */
     StockField_InceptionDate(47, "inceptionDate"),
-    /** 申购费用 - ETF */
+    /** Purchase fee - ETF */
     StockField_CreationFee(48, "creationFee"),
-    /** 管理费用 - ETF */
+    /** Management Fee - ETF */
     StockField_ManagementFee(49, "managementFee"),
-    /** 成分股Top10 占比 - ETF */
+    /** Proportion of top 10 constituent stocks - ETF */
     StockField_Top10_Composition_Rate(50, "Top10CompoRate"),
-    /** 成分股Top15 占比 - ETF */
+    /** Proportion of Top 15 Constituent Stocks - ETF */
     StockField_Top15_Composition_Rate(51, "Top15CompoRate"),
-    /** 成分股Top20 占比 - ETF */
+    /** Proportion of Top 20 Constituent Stocks - ETF */
     StockField_Top20_Composition_Rate(52, "Top20CompoRate"),
-    /** 溢价率(折扣率) - ETF */
+    /** Premium rate (discount rate) - ETF */
     StockField_DiscountPremium(53, "discountPremium"),
-    /** 资产规模-净值 - ETF */
+    /** asset size - net worth - ETF */
     StockField_Net_Worth_Aum(55, "aum"),
-    /** 资产规模-现价 - ETF */
+    /** Asset size - current price - ETF */
     StockField_assetSize(56, "assetSize"),
-    /** 振幅 */
+    /** Amplitude */
     StockField_Amplitude(57, "Amplitude"),
-    /** 盘前涨跌幅 */
+    /** Pre-market change rate */
     StockField_Pre_ChangeRate(58, "preChangeRate"),
-    /** 盘中涨跌幅 */
+    /** Intraday change rate */
     StockField_current_ChangeRate(59, "curChangeRate"),
-    /** 盘后涨跌幅 */
+    /** After-market change rate */
     StockField_Post_ChangeRate(60, "postChangeRate"),
-    /** 成分变动 - etf */
+    /** Component change - ETF */
     StockField_ETF_LastHoldingChangeDay(61, "LastHoldingChangeDay"),
-    /** 持仓数量 - etf */
+    /** Holding count - ETF */
     StockField_ETF_HoldingCount(62, "etfHoldingCount"),
-    /** 净利润 不带周期 */
+    /** Net income without cycle */
     StockField_Net_Income(63, "netIncomeVal"),
     ;
 
