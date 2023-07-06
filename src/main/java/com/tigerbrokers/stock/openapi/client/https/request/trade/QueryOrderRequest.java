9c79d240818d389da8229c27543345dd3be85d0f
liutongping <liutongping@itiger.com>
2023-07-06 17:32:06 +0800

    fix QueryOrderRequest default construct

--- src/main/java/com/tigerbrokers/stock/openapi/client/https/request/trade/QueryOrderRequest.java
+++ src/main/java/com/tigerbrokers/stock/openapi/client/https/request/trade/QueryOrderRequest.java
@@ -14,7 +14,7 @@ import java.time.format.DateTimeFormatter;
 public class QueryOrderRequest extends TigerCommonRequest implements TigerRequest<BatchOrderResponse> {
 
   public QueryOrderRequest() {
-    new QueryOrderRequest(MethodName.ORDERS);
+    this(MethodName.ORDERS);
   }
 
   public QueryOrderRequest(MethodName methodName) {
